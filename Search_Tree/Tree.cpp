f88c8df7adb20f2c6fc11200d7a62ccf57b252c3
IvanAlehin <ivanalehin236@gmail.com>
2024-02-12 23:11:29 +0400

    Добавьте файлы проекта.

---
+++ Search_Tree/Tree.cpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <stdexcept> 
+#include <random>
+
+using namespace std;
+
---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include "Search_Tree/Tree.cpp"
+
+using namespace std;
+
+int main() {
+
+    return 0;
+
+
+}
